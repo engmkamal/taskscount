e50ad11cbc129095e8b7962ac7bfd3212af42f90
kamal <engmkbd@gmail.com>
2023-05-15 11:50:35 +0600

    Page loader added

--- src/webparts/taskscount/components/Taskscount.tsx
+++ src/webparts/taskscount/components/Taskscount.tsx
@@ -13,6 +13,9 @@ import "@pnp/sp/items";
 import "@pnp/sp/items/get-all";
 import "@pnp/sp/site-users/web";
 
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 export interface IAuthor {
   Id: number;
   Title: string;
@@ -40,11 +43,15 @@ export interface IFile {
 
 export interface IPnPjsV3State {
   items: IFile[];
+  isLoading?: boolean;
+  percentage?: number;
 }
 
 export class PnPjsV3State implements IPnPjsV3State {
   constructor(
-    public items: IFile[] = []
+    public items: IFile[] = [],
+    public isLoading = true,
+    public percentage = 50,
   ) { }
 }
 
@@ -52,6 +59,7 @@ export default class Taskscount extends React.Component<ITaskscountProps, IPnPjs
 
   private LIST_NAME: string = "Tasks";
   private _sp: SPFI;
+  private intervalId:any;
 
   constructor(props: ITaskscountProps) {
     super(props);
@@ -59,127 +67,103 @@ export default class Taskscount extends React.Component<ITaskscountProps, IPnPjs
     this._sp = getSP(props.context);
   }
 
+  timer() {
+    this.setState({
+      percentage: this.state.percentage + 5
+    })
+    if(this.state.percentage > 90) { 
+      clearInterval(this.intervalId);
+    }
+  }
+
   public componentDidMount(): void {
+    
     this._readAllItems().then((items) => { console.log() }).catch(err => console.error(err));
-    this._readAllItems2().then((items) => { console.log() }).catch(err => console.error(err));
-    this._readAllItems3().then((items) => { console.log() }).catch(err => console.error(err));
-    this._readAllItems4().then((items) => { console.log() }).catch(err => console.error(err));
+    //this._readAllItems2().then((items) => { console.log() }).catch(err => console.error(err));
+    this.intervalId = setInterval(this.timer.bind(this), 100);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
   }
 
   private _readAllItems = async (): Promise<void> => {
     let items: any[] =[];
+    let isLoading = true ;
+
     try {
 
        const user = await this._sp.web.currentUser();
 
-       const userId = await user.Id;
-
+       const userId = await user.Id;  
+       
       const response: any[] = await this._sp.web.lists.getByTitle(this.LIST_NAME).items
       .orderBy("ID", false)
       .filter("Status eq 'Pending' and (AssignedToId ne null or DelegateUserId ne null)")
-      .select("AssignedToId", "DelegateUserId", "Status")
-      .top(100)();
+      .select("ID", "AssignedToId", "DelegateUserId", "Status")      
+      .top(10000)()
+      
+      // const response: any[] = await this._sp.web.lists.getByTitle(this.LIST_NAME).items
+      //  .select("AssignedToId", "DelegateUserId", "Status")
+      //  .getAll()
 
-      items = await (response.filter((item: any) => {
+       items = await (response.filter((item: any) => {
+ 
+         return (item.Status == 'Pending' && (item.AssignedToId != null && item.AssignedToId.includes(userId ))
+         || item.Status == 'Pending' && (item.DelegateUserId != null && item.DelegateUserId.includes(userId)))
+ 
+       }));
 
-        return (item.Status == 'Pending' && item.AssignedToId != null && item.AssignedToId.includes(userId )
-        || item.Status == 'Pending' && item.DelegateUserId != null && item.DelegateUserId.includes(userId))
-
-      }));
+       clearInterval(this.intervalId);
 
       this.setState({ items });
+
+      isLoading = false;      
+
+      this.setState({ isLoading });
 
     } catch (err) {
       console.error(`Error - ${JSON.stringify(err)} - `);
     }
   }
 
-  private _readAllItems2 = async (): Promise<void> => {
-    let items: any[] =[];
-    try {
+  // private _readAllItems2 = async (): Promise<void> => {
+  //   let items: any[] =[];
+  //   try {
 
-       const user = await this._sp.web.currentUser();
+  //      const user = await this._sp.web.currentUser();
 
-       const userId = await user.Id;
+  //      const userId = await user.Id;
 
-      const response2: any[] = await this._sp.web.lists.getByTitle(this.LIST_NAME).items
-      .orderBy("ID", false)
-      .filter("Status eq 'Pending' and (AssignedToId ne null or DelegateUserId ne null)")
-      .select("AssignedToId", "DelegateUserId", "Status")      
-      .top(500)()
+  //     const response2: any[] = await this._sp.web.lists.getByTitle(this.LIST_NAME).items
+  //     .orderBy("ID", false)
+  //     .filter("Status eq 'Pending' and (AssignedToId ne null or DelegateUserId ne null)")
+  //     .select("AssignedToId", "DelegateUserId", "Status")      
+  //     .top(500)()
 
-      items = await (response2.filter((item: any) => {
+  //     items = await (response2.filter((item: any) => {
 
-        return (item.Status == 'Pending' && item.AssignedToId != null && item.AssignedToId.includes(userId )
-        || item.Status == 'Pending' && item.DelegateUserId != null && item.DelegateUserId.includes(userId))
+  //       return (item.Status == 'Pending' && item.AssignedToId != null && item.AssignedToId.includes(userId )
+  //       || item.Status == 'Pending' && item.DelegateUserId != null && item.DelegateUserId.includes(userId))
 
-      }));
+  //     }));
 
-      this.setState({ items });
+  //     this.setState({ items });
 
-    } catch (err) {
-      console.error(`Error - ${JSON.stringify(err)} - `);
-    }
-  }
-
-  private _readAllItems3 = async (): Promise<void> => {
-    let items: any[] =[];
-    try {
-
-       const user = await this._sp.web.currentUser();
-
-       const userId = await user.Id;
-      const response3: any[] = await this._sp.web.lists.getByTitle(this.LIST_NAME).items
-      .orderBy("ID", false)
-      .filter("Status eq 'Pending' and (AssignedToId ne null or DelegateUserId ne null)")
-      .select("AssignedToId", "DelegateUserId", "Status")      
-      .top(5000)()
-
-      items = await (response3.filter((item: any) => {
-
-        return (item.Status == 'Pending' && item.AssignedToId != null && item.AssignedToId.includes(userId )
-        || item.Status == 'Pending' && item.DelegateUserId != null && item.DelegateUserId.includes(userId))
-
-      }));
-
-      this.setState({ items });
-
-    } catch (err) {
-      console.error(`Error - ${JSON.stringify(err)} - `);
-    }
-  }
-
-  private _readAllItems4 = async (): Promise<void> => {
-    let items: any[] =[];
-    try {
-
-       const user = await this._sp.web.currentUser();
-
-       const userId = await user.Id;
-
-      const response3: any[] = await this._sp.web.lists.getByTitle(this.LIST_NAME).items
-      .select("AssignedToId", "DelegateUserId", "Status")
-      .getAll()     
-
-      items = await (response3.filter((item: any) => {
-
-        return (item.Status == 'Pending' && (item.AssignedToId != null && item.AssignedToId.includes(userId ))
-        || item.Status == 'Pending' && (item.DelegateUserId != null && item.DelegateUserId.includes(userId)))
-
-      }));
-
-      this.setState({ items });
-
-    } catch (err) {
-      console.error(`Error - ${JSON.stringify(err)} - `);
-    }
-  }
+  //   } catch (err) {
+  //     console.error(`Error - ${JSON.stringify(err)} - `);
+  //   }
+  // }
 
   public render(): React.ReactElement<ITaskscountProps> {
 
     return (
+
+
         
       <div className={styles.container} >
+        
+
         <a style={{textDecoration: 'none'}} className={styles.links} href="https://banglalinkdigitalcomm.sharepoint.com/sites/vloungeonline/SitePages/Pending-Task.aspx">
           <div className={styles.flexContainer} >
           
@@ -189,16 +173,29 @@ export default class Taskscount extends React.Component<ITaskscountProps, IPnPjs
 
             <div className={styles.flexChildTxt}>
               <div >
-                <span className={styles.text} ><b>Pending Task(s): </b></span>
+                <span className={styles.text} >Pending Task(s): </span>
               </div>
-            </div>
+            </div>           
 
-            <div className={styles.flexCount}>                  
+            <div className={styles.flexCount}>
               <div className={styles.fieldsetDiv}>
-                <fieldset className={styles.fieldsetBrdr}>
-                  <span className={styles.text}><b>{this.state.items.length} </b></span>
-                </fieldset>                                          
-              </div> 
+                {this.state.isLoading ? (
+                  <div className={styles.skCubeGrid}>
+                    <div className="progress-container">
+                      <CircularProgressbar value={this.state.percentage} text={`${this.state.percentage}%`} />
+                    </div>
+                    <p style={{fontSize: '14px', color:'whitesmoke', marginTop: '0px'}}>Loading...</p>
+                  </div>
+                ) : (
+                  <div >                  
+                    
+                      <fieldset className={styles.fieldsetBrdr}>
+                        <span className={styles.text}>{this.state.items.length} </span>
+                      </fieldset>                                          
+                     
+                  </div>
+                )}
+              </div>
             </div>
 
           </div>
